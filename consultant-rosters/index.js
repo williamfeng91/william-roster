1e2706d49cbe714c97d7495fa38b01dda231ce12
William Feng <william.feng91@gmail.com>
2018-07-20 13:37:32 +1000

    Committed from bappo.com

---
+++ consultant-rosters/index.js
@@ -0,0 +1,110 @@
+import React from "react";
+import {
+  View,
+  Text,
+  styled,
+  Button,
+  ScrollView,
+  TextInput
+} from "bappo-components";
+import SingleRoster from "single-roster";
+
+class Consultants extends React.Component {
+  state = {
+    consultants: [],
+    shortlist: [],
+    searchString: "test"
+  };
+
+  getData = async () => {
+    const consultants = await this.props.$models.Consultant.findAll({
+      limit: 1000,
+      where: {
+        active: true
+      }
+    });
+    this.setState({ consultants, shortlist: consultants });
+  };
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState === this.state) return false;
+    return true;
+  }
+
+  refreshList = () => {
+    const searchString = this.state.searchString.toLocaleLowerCase();
+    const shortlist = this.state.consultants.filter(
+      c => c.name.toLowerCase().search(searchString) > -1
+    );
+    this.setState({ shortlist });
+  };
+
+  onSearchTermChange = v => {
+    this.setState({ searchString: v });
+    clearTimeout(this.timer);
+    this.timer = setTimeout(this.refreshList, 300);
+  };
+
+  showRoster = consultant => {
+    this.props.$navigation.navigate("SingleRosterPage", {
+      recordId: consultant.id
+    });
+  };
+
+  render() {
+    return (
+      <Container>
+        <SearchInput
+          placeholder="Search consultants"
+          onValueChange={this.onSearchTermChange}
+          value={this.state.searchString}
+        />
+        <ScrollView>
+          {this.state.shortlist.map(c => (
+            <Row onPress={() => this.showRoster(c)} key={c.id}>
+              <Text>{c.name}</Text>
+            </Row>
+          ))}
+        </ScrollView>
+      </Container>
+    );
+  }
+}
+
+export default Consultants;
+
+const Row = styled(Button)`
+  height: 40px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Container = styled(View)`
+  display: flex;
+  flex: 1;
+`;
+
+const RosterContainer = styled(View)`
+  display: flex;
+  flex: 1;
+  padding-bottom: 50px;
+`;
+
+const BackButton = styled(Button)`
+  height: 40px;
+  justify-content: center;
+`;
+
+const SearchInput = styled(TextInput)`
+  border-style: solid;
+  border-width: 2px;
+  border-color: #ddd;
+  border-radius: 3px;
+  margin: 20px;
+  height: 40px;
+  padding-left: 20px;
+`;
